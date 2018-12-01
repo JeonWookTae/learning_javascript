9983a7d9c0a5e67281fa5c9d5ef12b7b730a7772
JeonWookTae <pkgg011@naver.com>
2018-12-01 21:04:49 +0900

    chapter9

---
+++ chapter9/oop.js
@@ -0,0 +1,19 @@
+class Car{
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+        this.userGears = ['P', 'N', 'R', 'D'];
+        this.userGear = this.userGears[0];
+    }
+    shift(gear){
+        if(this.userGears.indexOf(gear) < 0){
+            throw new Error(`Invalid gear ${gear}`);
+        }
+        this.userGear = gear;
+    }
+}
+
+const car1 = new Car();
+const car2 = new Car();
+console.log(car1 instanceof Car);
+console.log(car2 instanceof Car);
