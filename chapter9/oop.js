38d5fe5fc52bbd15a6f3d82952db8cb6c0950e72
JeonWookTae <pkgg011@naver.com>
2018-12-03 23:43:59 +0900

    chapter9

--- chapter9/oop.js
+++ chapter9/oop.js
@@ -1,14 +1,27 @@
-class Car{
+const SYM = Symbol();
+const o = {a: 1, b: 2, c: 3, [SYM]: 4};
+Object.keys(o).forEach(prop => console.log(`${prop}: ${o[prop]}`));
+
+class Car {
     constructor(make, model) {
         this.make = make;
         this.model = model;
-        this.userGears = ['P', 'N', 'R', 'D'];
-        this.userGear = this.userGears[0];
+        this._userGears = ['P', 'N', 'R', 'D'];
+        this._userGear = this._userGears[0];
     }
-    shift(gear){
-        if(this.userGears.indexOf(gear) < 0){
-            throw new Error(`Invalid gear ${gear}`);
+
+    get userGear() {
+        return this._userGear;
+    }
+
+    set userGear(value) {
+        if (this._userGears.indexOf(value) < 0) {
+            throw new Error(`Invalid gear: ${value}`);
         }
+        this._userGear = value;
+    }
+
+    shift(gear) {
         this.userGear = gear;
     }
 }
@@ -17,3 +30,36 @@ const car1 = new Car();
 const car2 = new Car();
 console.log(car1 instanceof Car);
 console.log(car2 instanceof Car);
+
+car1.shift('D');
+car2.shift('R');
+
+console.log(car1.userGear, car2.userGear);
+
+const newCar = (function () {
+    const carProps = new WeakMap();
+
+    class Car {
+        constructor(make, model) {
+            this.make = make;
+            this.model = model;
+            this._userGear = ['P', 'N', 'R', 'D'];
+            carProps.set(this, {userGear: this._userGears[0]})
+        }
+
+        get userGear() {
+            return carProps.get(this).userGear;
+        }
+
+        set userGear(value) {
+            if (this._userGear.indexOf(value) < 0) {
+                throw new Error(`Invalid gear: ${value}`);
+            }
+            carProps.get(this).userGear = value;
+        }
+
+        shift(gear){this.userGear = gear;}
+
+    }
+    return Car;
+})();
