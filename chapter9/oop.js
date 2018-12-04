67a3fd9d0363e502e492913381afd4ad8c641788
JeonWookTae <pkgg011@naver.com>
2018-12-04 23:26:41 +0900

    chapter9

--- chapter9/oop.js
+++ chapter9/oop.js
@@ -64,3 +64,13 @@ const newCar = (function () {
     }
     return Car;
 })();
+
+function CarFunc(make, model){
+    this.make = make;
+    this.model = model;
+    this._userGears = ['P', 'N', 'R', 'D'];
+    this._userGear = this.userGears[0];
+}
+class Es6Car {}
+function Es5Car(){}
+console.log(typeof(Es6Car), typeof(Es5Car) );
