0328be6c68c0997bc18020231337bad67f899c76
JeonWookTae <pkgg011@naver.com>
2018-12-19 00:45:39 +0900

    chapter14

--- chapter14/generator.js
+++ chapter14/generator.js
@@ -1,1 +1,16 @@
 //generator
+
+function nfcall(f, ...args){
+    return new Promise(function(resolve, reject){
+        f.call(null, ...args, function(err, ...args){
+            if(err) return reject(err);
+            resolve(args.length<2? args[0]:args);
+        });
+    });
+}
+
+function ptimeout(delay){
+    return new Promise(function(resolve, reject){
+        setTimeout(resolve, reject);
+    });
+}
