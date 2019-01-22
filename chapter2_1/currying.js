68c4d8e2203192948e96479c5b2895a98e56eb00
JeonWookTae <pkgg011@naver.com>
2019-01-23 01:21:26 +0900

    chapter2_1

---
+++ chapter2_1/currying.js
@@ -0,0 +1,23 @@
+const getFakeMembers = count => new Promise((resolve, rejects) =>{
+    const api = `https://api.randomuser.me/?nat=US&results=${count}`;
+    const request = new XMLHttpRequest();
+    request.open('GET', api);
+    request.onload = () =>
+        (request.status === 200) ?
+            resloves(JSON.parse(request.response).results):
+            reject(Error(request.statusText));
+    request.onerror = (err) => rejects(err);
+    request.send()
+});
+
+const userLogs = userName => message =>
+    console.log(`${userName} -> ${message}`);
+
+const log = userLogs("grandpa23");
+
+log("attempted to load 20 fake members");
+
+getFakeMembers(20).then(
+    members => log(`successfully loaded ${members.length} members`),
+    error => log(`encountered an error loading member`)
+);
