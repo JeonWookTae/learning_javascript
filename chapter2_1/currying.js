5350ba572794519e2655241f146a098d2624b735
JeonWookTae <pkgg011@naver.com>
2019-01-24 08:02:56 +0900

    chapter2_1

--- chapter2_1/currying.js
+++ chapter2_1/currying.js
@@ -4,8 +4,8 @@ const getFakeMembers = count => new Promise((resolve, rejects) =>{
     request.open('GET', api);
     request.onload = () =>
         (request.status === 200) ?
-            resloves(JSON.parse(request.response).results):
-            reject(Error(request.statusText));
+            resolve(JSON.parse(request.response).results):
+            rejects(Error(request.statusText));
     request.onerror = (err) => rejects(err);
     request.send()
 });
@@ -21,3 +21,9 @@ getFakeMembers(20).then(
     members => log(`successfully loaded ${members.length} members`),
     error => log(`encountered an error loading member`)
 );
+
+const countdown = (value, fn) => {
+    fn(value);
+    return (value > 0) ? countdown(value-1, fn) : value
+};
+countdown(10, value => console.log(value));
